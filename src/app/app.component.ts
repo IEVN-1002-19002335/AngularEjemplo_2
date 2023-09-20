4f33aea7a19a93f2db38d969f215806ea93b916b
Karla Martinez <19002335@alumnos.utleon.edu.mx>
2023-09-19 23:40:56 -0600

    19-09-23 KJMA

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularEjemplo_02';
+  duplicarNumero(n:number):number{
+    return n+2;
+}
+
+pelicula={
+    titulo:'Cantinflas show',
+    fechadeLanzamiento:new Date(),
+    precio:1234
+}
+}
