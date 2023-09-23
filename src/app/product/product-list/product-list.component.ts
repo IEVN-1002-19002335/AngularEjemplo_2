cd0f4f1e80def61d5b7b0ca288aa1bf6c209d914
19002335@alumnos.utleon.edu.mx <19002335@alumnos.utleon.edu.mx>
2023-09-23 11:32:49 -0600

    23-09-23 KJMA

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Iproducto } from '../iproducto';
 
 @Component({
   selector: 'app-product-list',
@@ -12,7 +13,11 @@ export class ProductListComponent {
     muestraImg:boolean=true;
     listFilter:string='';
 
-    productos:any[]=[
+    showImage():void{
+      this.muestraImg=!this.muestraImg;
+    }
+
+    productos:Iproducto[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
@@ -38,7 +43,7 @@ export class ProductListComponent {
       "productoId":3,
       "Modelo":"Rio",
       "Descripcion":"2 puertas familiar",
-      "year":"Enero 2 20223",
+      "year":"marzo 2022",
       "precio":200000,
       "Marca":"KIA",
       "Color":"Azul",
